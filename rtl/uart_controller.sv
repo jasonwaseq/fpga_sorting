3aa76cf1a16ad26789afd69253640c8ae738858d
Jason Waseq <103277116+jasonwaseq@users.noreply.github.com>
2025-08-20 00:16:53 +0000

    uart_controller handshake logic

--- rtl/uart_controller.sv
+++ rtl/uart_controller.sv
@@ -1,71 +1,58 @@
-module uart_controller(
-    input clk_i, 
-    input rst_i, 
-    input pc_txdata_i,
-    input pc_rxready_i,
-    input pc_txready_i, 
-    input pc_txvalid_i, 
-    input [7:0] pc_data_i, 
-    output logic pc_txdata_o
-    );
-    
-    uart_rx
-    #(
-        .CLK_FREQ(100000000), // 100 MHz
-        .BAUD_RATE(115200)
-    ) uart_rx_inst (
-        .clk(clk_i),
-        .rst(rst_i),
-        .rxd(rxd),
-        .overrun_error(),
-        .frame_error(),
+module uart_controller (
+    input  logic        clk_i,
+    input  logic        rst_i,
+    // PC Interface
+    input  logic       pc_txvalid_i, // PC has data available
+    input  logic [7:0] pc_txdata_i,  // Data from PC
+    input  logic       pc_rxready_i, // PC ready to accept
+    output logic       pc_rxvalid_o, // Controller has data for PC
+    output logic       pc_txready_o, // Controller ready to accept from PC
+    output logic [7:0] pc_rxdata_o   // Data to PC
+);
+
+    logic        rxd;
+    logic        txd;
+
+    logic [7:0]  rx_data;
+    logic        rx_valid;
+    logic        rx_ready;
+
+    logic [7:0]  tx_data;
+    logic        tx_valid;
+    logic        tx_ready;
+
+    // ---------------- UART Receiver ----------------
+    uart_rx uart_rx_inst (
+        .clk_i (clk_i),
+        .rst_i (rst_i),
+        .rxd_i (rxd),
+        .data_o (rx_data),
+        .valid_o(rx_valid),
+        .ready_i(rx_ready),
     );
 
-    uart_tx 
-    #(
-        .CLK_FREQ(100000000), // 100 MHz
-        .BAUD_RATE(115200)
-    ) uart_tx_inst (
-        .clk_i(clk_i),
-        .rst_i(rst_i),
-        .txd(tx_data),
-        .busy(fpga_txready),
+    // ---------------- UART Transmitter ----------------
+    uart_tx uart_tx_inst (
+        .clk_i (clk_i),
+        .rst_i (rst_i),
+        .data_i (tx_data),
+        .valid_i(tx_valid),
+        .ready_o(tx_ready),
+        .txd_o (txd)
     );
-    
-    wire fpga_txd;
-    wire fpga_txvalid;
-    wire fpga_rxready;
-    wire fpga_txready;
-    wire fpga_rxvalid;
-    wire [7:0] rx_processed;
-    logic rxd;
-    logic [7:0] tx_data;
-    
-    wire pc_rx_handshake;
-    assign pc_rx_handshake = pc_txready_i & fpga_rxready;
-    
-    always_comb
-    begin
-    if(pc_rx_handshake)
-    rxd = pc_txdata_i;
-    end
-    
-    wire rx_tx_handshake;
-    assign rx_tx_handshake = fpga_txready & fpga_rxready & fpga_rxvalid;
-    
-    always_comb
-    begin
-    if(rx_tx_handshake)
-    tx_data = rx_processed;
-    end
-    
-    wire tx_pc_handshake;
-    assign tx_pc_handshake = pc_rxready_i & fpga_txready & fpga_txvalid;
+
+    // ---------------- Handshake Logic ----------------
+    // From PC to UART RX
+    assign rxd = pc_txvalid_i ? pc_txdata_i[0] : 1'b1;  
    
-    always_comb
-    begin
-    if(tx_pc_handshake)
-    pc_txd_o = fpga_txd;
-    end
+    // From UART RX to PC
+    assign pc_rxvalid_o = rx_valid;
+    assign pc_rxdata_o  = rx_data;
+    assign rx_ready     = pc_rxready_i;
 
-    endmodule
+    // From PC to UART TX
+    assign tx_valid     = pc_txvalid_i;
+    assign tx_data      = pc_txdata_i;
+    assign pc_txready_o = tx_ready;
+
+endmodule
